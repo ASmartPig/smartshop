fbc9e86c8d5f3626a2f6a68d626767bc47578eb7
liaoze <616655260@qq.com>
2018-12-25 14:09:37 +0800

    增加实体

---
+++ smartshop-dao/src/main/java/com/smart/pojo/User.java
@@ -0,0 +1,23 @@
+package com.smart.pojo;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class User {
+    private String id;
+    private String account;
+    private String password;
+    private String email;
+    private String phone;
+    private String headimg_url;
+    private int status;
+    private int sex;
+    private Date date_create;
+    private Date date_update;
+    private Date date_delete;
+    private String last_login_ip;
+    private int is_delete;
+
+}
