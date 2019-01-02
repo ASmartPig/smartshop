6433bf231c06ab031ae65a30318f522703b14bee
ASmartPig <616655260@qq.com>
2019-01-02 23:12:42 +0800

    小改动

--- smartshop-service/src/main/java/com/smart/service/serviceImpl/ProductServiceImpl.java
+++ smartshop-service/src/main/java/com/smart/service/serviceImpl/ProductServiceImpl.java
@@ -1,11 +1,13 @@
 package com.smart.service.serviceImpl;
 
+import com.smart.mapper.ProductMapper;
 import com.smart.pojo.Product;
 import com.smart.service.ProductService;
 
 import java.util.List;
 
 public class ProductServiceImpl implements ProductService {
+    private ProductMapper productMapper;
 
     @Override
     public List<Product> getUserByAccount() {
