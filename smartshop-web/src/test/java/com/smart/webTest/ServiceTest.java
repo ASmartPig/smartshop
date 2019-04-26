cb6a2491f49d27c021e80b063ba2b49bb4830dbb
liaoze <616655260@qq.com>
2019-04-26 15:31:57 +0800

    更改加载配置文件的结构

--- smartshop-web/src/test/java/com/smart/webTest/ServiceTest.java
+++ smartshop-web/src/test/java/com/smart/webTest/ServiceTest.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("classpath:spring-mybatis.xml")
+@ContextConfiguration("classpath:ApplicationContext.xml")
 public class ServiceTest {
 
     @Test
