1029ceb7b5710d4964a24829c0f494faf47975f7
liaoze <616655260@qq.com>
2019-04-29 18:49:30 +0800

    去除子项目中父项目已有的依赖，以及因为depenencyManagement管理，父项目依赖的版本号多余

--- smartshop-web/src/test/java/com/smart/webTest/ServiceTest.java
+++ smartshop-web/src/test/java/com/smart/webTest/ServiceTest.java
@@ -2,15 +2,21 @@ package com.smart.webTest;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:ApplicationContext.xml")
 public class ServiceTest {
-
+   private static  Logger logger = LoggerFactory.getLogger(ServiceTest.class);
     @Test
     public void test() {
 //        ApplicationContext act = new ClassPathXmlApplicationContext("classpath:spring-mvc.xml");
 //        System.out.println(act.containsBean("userInfoController"));
+
+        logger.info("test........");
+        logger.info("dfsdakjfaksdjfkjs");
+        logger.debug("sdafkaksdjfkajsdkl.....");
     }
 }
